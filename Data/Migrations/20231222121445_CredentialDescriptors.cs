eff1504cf0d548d932be14b098e3e16d1df9c706
ThomasGrant08 <thomas.grant@moirae.co.uk>
2023-12-22 12:18:19 +0000

    Credentials and SASS Styling

---
+++ Data/Migrations/20231222121445_CredentialDescriptors.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebPulse_WebManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CredentialDescriptors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Credential",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Credential",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Valid",
+                table: "Credential",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Credential");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Credential");
+
+            migrationBuilder.DropColumn(
+                name: "Valid",
+                table: "Credential");
+        }
+    }
+}
--- Models/Credential.cs
+++ Models/Credential.cs
@@ -7,11 +7,12 @@ namespace WebPulse_WebManager.Models
         #region Properties
         public string Username { get; set; }
         public string Password { get; set; }
-
+        public string Title { get; set; }
+        public string Description { get; set; }
         public int WebsiteId { get; set; }
         public virtual Website Website { get; set; }
-
         public virtual List<ApplicationUser> AssignedUsers { get; set; }
+        public bool Valid { get; set; }
 
         #endregion
     }
