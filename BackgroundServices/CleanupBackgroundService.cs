59b17e1d28b0d4e5602ed2df20485a79b4b8c16a
ThomasGrant08 <thomas.grant@moirae.co.uk>
2023-12-22 10:52:04 +0000

    Cleanup Service

---
+++ BackgroundServices/CleanupBackgroundService.cs
@@ -0,0 +1,40 @@
+﻿using WebPulse_WebManager.Services;
+
+namespace WebPulse_WebManager.BackgroundServices
+{
+    public class CleanupBackgroundService : BackgroundService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger<CleanupBackgroundService> _logger; 
+
+        public CleanupBackgroundService(IServiceProvider serviceProvider, ILogger<CleanupBackgroundService> logger)
+        {
+            _serviceProvider = serviceProvider;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while(!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    using (var scope = _serviceProvider.CreateScope())
+                    {
+                        var cleanupService = scope.ServiceProvider.GetRequiredService<CleanupService>();
+                        cleanupService.CleanupOldRecords();
+
+                        _logger.LogInformation("Cleanup executed at: {time}", DateTimeOffset.Now);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error occurred executing cleanup");
+                }
+
+                await Task.Delay(TimeSpan.FromDays(7), stoppingToken);
+            }
+        }
+
+    }
+}
---
+++ Services/CleanupService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using WebPulse_WebManager.Data;
+using WebPulse_WebManager.Models;
+using WebPulse_WebManager.Utility;
+
+namespace WebPulse_WebManager.Services
+{
+    public class CleanupService
+    {
+
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<CleanupService> _logger;
+
+        public CleanupService(ApplicationDbContext context, ILogger<CleanupService> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public void CleanupOldRecords()
+        {
+
+            try
+            {
+                _logger.LogInformation("Cleanup started at: {time}", DateTimeOffset.Now);
+                var dbSets = _context.GetType().GetProperties()
+                .Where(p => p.PropertyType.IsGenericType && p.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>))
+                .ToList();
+
+                foreach (var dbSetProperty in dbSets)
+                {
+                    _logger.LogInformation("Checking {TableName} for records to delete.", dbSetProperty.Name);
+                    var entityType = dbSetProperty.PropertyType.GetGenericArguments()[0];
+
+                    var deletedAtProperty = entityType.GetProperty("DeletedAt");
+
+                    if (deletedAtProperty != null)
+                    {
+                        var recordsToDelete = ((IQueryable<object>)dbSetProperty.GetValue(_context))
+                            .AsEnumerable() // Switch to client evaluation
+                            .Where(e =>
+                                (DateTime?)deletedAtProperty.GetValue(e) != null &&
+                                (DateTime?)deletedAtProperty.GetValue(e) < DateTime.UtcNow.AddWeeks(-4)
+                            )
+                            .ToList();
+
+                        _logger.LogInformation("Found {RecordCount} records in {TableName} to delete.", recordsToDelete.Count, entityType.Name);
+                        // Delete the records
+                        _context.RemoveRange(recordsToDelete.Cast<object>());
+                        _logger.LogInformation("{RecordCount} records deleted from {TableName}.", recordsToDelete.Count, entityType.Name);
+
+                    }
+
+
+                }
+
+                _context.SaveChanges();
+
+                _logger.LogInformation("Cleanup finished at: {time}", DateTimeOffset.Now);
+
+            } catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred executing cleanup");
+            }
+            
+        }
+
+    }
+}
