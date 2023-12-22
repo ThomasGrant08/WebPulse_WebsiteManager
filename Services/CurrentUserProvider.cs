83767ef4f3e5ffd81529b56f43397e8339e56563
ThomasGrant08 <thomas.grant@moirae.co.uk>
2023-12-22 10:59:15 +0000

    Current User Provider

---
+++ Services/CurrentUserProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using WebPulse_WebManager.Models;
+
+namespace WebPulse_WebManager.Services
+{
+    public interface ICurrentUserProvider
+    {
+        ApplicationUser? CurrentUser { get; }
+    }
+
+    public class CurrentUserProvider : ICurrentUserProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CurrentUserProvider(IHttpContextAccessor httpContextAccessor, UserManager<ApplicationUser> userManager)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _userManager = userManager;
+        }
+
+        public ApplicationUser? CurrentUser
+        {
+            get
+            {
+                var userId = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                return !string.IsNullOrEmpty(userId) ? _userManager.FindByIdAsync(userId).Result : null;
+            }
+        }
+    }
+}
